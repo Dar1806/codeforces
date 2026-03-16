5e637800d237a35f5fd5300a896cf0e4de809ba0
Dar1806 <noemeuniercoli@gmail.com>
2026-03-16 18:36:20 +0100

    add routine

---
+++ cp31/Line_trip.py
@@ -0,0 +1,14 @@
+for caca in range(int(input())):
+	last_nbr = int(input().split(" ")[1])
+	n = list(map(int, input().split(" ")))
+	liters = 0
+	if len(n) == 1:
+		liters = n[0]
+	for nlist in range(1, len(n)):
+		temp = n[nlist] - n[nlist - 1]
+		if temp > liters:
+			liters = temp
+	nlist = len(n) - 1
+	if liters < (last_nbr - n[nlist]) * 2:
+		liters = (last_nbr - n[nlist]) * 2
+	print(liters)
